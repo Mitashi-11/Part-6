f53ccca75f8f6185861d17511e2bd129e2fafde3
mitashi-11 <mitashipari@gmail.com>
2021-03-09 23:12:34 -0800

    Updated for Task2

--- fade/fade.py
+++ fade/fade.py
@@ -1,6 +1,7 @@
 from typing import List
 import math
 import file_funcs
+import sys
 
 
 def read_commandline(argv: List[str]):
@@ -16,43 +17,50 @@ def read_commandline(argv: List[str]):
 
 
 def create_fade(pixel: List[float], write_file, row, col, radius, pixel_row, pixel_col):
-    distance = math.sqrt((row - pixel_row)**2 + (col - pixel_col)**2)
+    distance = math.sqrt((pixel_row - row)**2 + (pixel_col - col)**2)
     scale = (radius-distance)/radius
     if scale < 0.2:
         scale = 0.2
-    red = pixel[0] * scale
-    green = pixel[1] * scale
-    blue = pixel[2] * scale
+    red = int(pixel[0] * scale)
+    green = int(pixel[1] * scale)
+    blue = int(pixel[2] * scale)
     print(red, green, blue, file=write_file)
 
 
 def read_file(ifile, ofile, row, col, radius):
     pixel = []
     ctr = 0
-    line_ctr = 1
     pixel_row = 0
     pixel_col = 0
-    width = 0
-    for line in ifile:
-        split_line = line.split()
-        if line_ctr == 2:
-            width = split_line[0]
-            height = split_line[1]
-            print('P3', file=ofile)
-            print(width, height, file=ofile)
-            print('255', file=ofile)
-        line_ctr += 1
-        for value in split_line:
-            try:
-                pixel.append(int(value))
-                ctr += 1
-                pixel_col += 1
-                if pixel_col == width:
-                    pixel_row += 1
-                    pixel_col = 0
-            except ValueError:
-                pass
-            if ctr == 3:
-                create_fade(pixel, ofile, row, col, radius, pixel_row, pixel_col)
-                ctr = 0
-                pixel = []
+    file_content = (ifile.read()).split()
+    print('P3', file=ofile)
+    print(file_content[1], file_content[2], file=ofile)
+    print('255', file=ofile)
+    width = int(file_content[1])
+    for i in range(4, len(file_content)):
+        try:
+            pixel.append(int(file_content[i]))
+            ctr += 1
+        except ValueError:
+            pass
+        if ctr == 3:
+            if pixel_col == width:
+                pixel_row += 1
+                pixel_col = 0
+            #print(pixel, pixel_row, pixel_col, row, col)
+            create_fade(pixel, ofile, row, col, radius, pixel_row, pixel_col)
+            ctr = 0
+            pixel = []
+            pixel_col += 1
+
+
+def main(argv: List[str]):
+    comdline_args = read_commandline(argv)
+    write_file = file_funcs.open_file('fadded.ppm', 'w')
+    read_file(comdline_args[0], write_file, comdline_args[1], comdline_args[2], comdline_args[3])
+    comdline_args[0].close()
+    write_file.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
